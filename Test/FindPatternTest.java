61c3d179deea8f6f31fa72769448a7a22225968e
Toby Readman <treadm01@dcs.bbk.ac.uk>
2018-02-04 20:37:50 +0000

    getting a list of repeats

--- Test/FindPatternTest.java
+++ Test/FindPatternTest.java
@@ -3,10 +3,16 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class FindPatternTest {
+    FindPattern fp = new FindPattern();
+
 
     @Test
-    public void getRepeat() {
-        FindPattern fp = new FindPattern();
-        assertEquals("bc", fp.getRepeat("abcdbc"));
+    public void getRepeatOfPairs() {
+        assertEquals("bc", fp.getRepeat("abcdbc").get(0));
+        assertEquals("ac", fp.getRepeat("accdac").get(0));
+        assertEquals("cc", fp.getRepeat("abdccbecc").get(0));
+        assertEquals("cc", fp.getRepeat("cccc").get(0));
+        assertEquals("cc", fp.getRepeat("ccccd").get(0));
+        assertEquals("cc", fp.getRepeat("ccccde").get(0));
     }
 }
--- src/FindPattern.java
+++ src/FindPattern.java
@@ -1,30 +1,39 @@
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FindPattern {
-    private Map<String, String> grammars = new HashMap<>();
-    private Map<String, Integer> patternList = new HashMap<>();
+    private Map<String, String> grammars;
+    private Map<String, Integer> patternList;
+    private List<String> patterns;
 
-    public String getRepeat(String input) {
+    public List<String> getRepeat(String input) {
+        patternList = new HashMap<>();
+        patterns = new ArrayList<>();
         String found = null;
-        for (int i = 0; i < input.length(); i++ ) {
+
+        for (int i = 0; i < input.length()-1; i++ ) {
             String s = input.substring(i, i + 2);
-            System.out.println(s);
-            patternList.putIfAbsent(s, 1);
 
             if (patternList.containsKey(s)) {
                 patternList.put(s, patternList.get(s) + 1);
             }
+            patternList.putIfAbsent(s, 1);
         }
 
         for (String s : patternList.keySet()) {
             if (patternList.get(s) > 1) {
-                found = s;
+                patterns.add(s);
             }
         }
 
-        return found;
+        return patterns;
+    }
+
+    public void initGrammar(String input) {
+        grammars = new HashMap<>();
+        grammars.putIfAbsent("S", input);
     }
 }

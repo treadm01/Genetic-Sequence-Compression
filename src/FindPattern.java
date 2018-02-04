5a3e43ecf63702de03ace82aadd739a332859322
Toby Readman <treadm01@dcs.bbk.ac.uk>
2018-02-04 20:04:48 +0000

    trying to split up to pairs and check for repeats

--- src/FindPattern.java
+++ src/FindPattern.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class FindPattern {
     private Map<String, String> grammars = new HashMap<>();
+    private Map<String, Integer> patternList = new HashMap<>();
 
     public String getRepeat(String input) {
+        String found;
+        for (int i = 0; i < input.length(); i++ ) {
+            String s = input.substring(i, i + 1);
+            patternList.add(s);
+        }
+
+
         return "";
     }
 }

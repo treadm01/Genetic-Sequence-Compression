c94dea5aa5e4bfdd21516562df2280cd684738e1
Toby Readman <treadm01@dcs.bbk.ac.uk>
2018-02-05 14:33:31 +0000

    checking patterns and keeping count for now

--- Test/FindPatternTest.java
+++ Test/FindPatternTest.java
@@ -5,43 +5,6 @@ import static org.junit.Assert.*;
 public class FindPatternTest {
     FindPattern fp = new FindPattern();
 
-
-    @Test
-    public void getRepeatOfPairs() {
-        assertEquals("bc", fp.getRepeat("abcdbc").get(0));
-        assertEquals("ac", fp.getRepeat("accdac").get(0));
-        assertEquals("cc", fp.getRepeat("abdccbecc").get(0));
-        assertEquals("cc", fp.getRepeat("cccc").get(0));
-        assertEquals("cc", fp.getRepeat("ccccd").get(0));
-        assertEquals("cc", fp.getRepeat("ccccde").get(0));
-    }
-
-    @Test
-    public void addPattern() {
-        fp.initGrammar("abcdbc");
-        fp.addPattern();
-        assertEquals("bc", fp.getGrammars().get("2"));
-
-        fp.initGrammar("abcdbcabcdbc");
-        fp.addPattern();
-        System.out.println(fp.getGrammars().toString());
-        assertEquals("aBdB", fp.getGrammars().get("2"));
-    }
-
-    @Test
-    public void initGrammar() {
-        fp.initGrammar("abcdbc");
-        System.out.println(fp.getGrammars().toString());
-    }
-
-    @Test
-    public void getRepeat() {
-    }
-
-    @Test
-    public void applyNonTerminals() {
-    }
-
     @Test
     public void getGrammars() {
     }
@@ -50,8 +13,36 @@ public class FindPatternTest {
     public void addSymbol() {
         String input = "abcdbc";
         for (int i = 0; i < input.length(); i++) {
-            fp.addSymbol(input.substring(i, i + 1));
+            fp.addSymbol("S", input.substring(i, i + 1));
         }
         System.out.println(fp.getGrammars().toString());
+    }
+
+    @Test
+    public void getDigram() {
+        String input = "ab";
+        for (int i = 0; i < input.length(); i++) {
+            fp.addSymbol("S", input.substring(i, i + 1));
+        }
+
+        assertEquals("ab", fp.getDigram());
+
+        input = "abcdefgh";
+        for (int i = 0; i < input.length(); i++) {
+            fp.addSymbol("S", input.substring(i, i + 1));
+        }
+
+        assertEquals("gh", fp.getDigram());
+
+    }
+
+    @Test
+    public void checkForPattern() {
+        String input = "abcdbc";
+        for (int i = 0; i < input.length(); i++) {
+            fp.addSymbol("S", input.substring(i, i + 1));
+        }
+
+        assertEquals(2, fp.checkForPattern(fp.getDigram()));
     }
 }
--- src/FindPattern.java
+++ src/FindPattern.java
@@ -1,66 +1,47 @@
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class FindPattern {
     private Map<String, String> grammars = new HashMap<>();;
-    private Map<String, Integer> patternList;
-    private List<String> patterns;
-    private Integer count;
 
-    public List<String> getRepeat(String input) {
-        // needs to be implemented properly, symbol by symbol checking, check all grammar, proper terminal symbols etc
-        patternList = new HashMap<>();
-        patterns = new ArrayList<>();
-        String found = null;
-
-        for (int i = 0; i < input.length()-1; i++ ) {
-            String s = input.substring(i, i + 2);
-
-            if (patternList.containsKey(s)) {
-                patternList.put(s, patternList.get(s) + 1);
-            }
-            patternList.putIfAbsent(s, 1);
-        }
-
-        for (String s : patternList.keySet()) {
-            if (patternList.get(s) > 1) {
-                patterns.add(s);
-            }
-        }
-
-        return patterns;
-    }
-
-    public void applyNonTerminals() {
-
-    }
-
-    public void addPattern() {
-        for (String x : getRepeat(grammars.get("1"))) {
-            count++;
-            grammars.putIfAbsent(count.toString(), x);
-        }
-    }
-
-    public void initGrammar(String input) {
-        count = 1;
-        grammars = new HashMap<>();
-        grammars.putIfAbsent("S", input.substring(0, 1));
-    }
 
     public Map<String, String> getGrammars() {
         return this.grammars;
     }
 
-    public void addSymbol(String input) {
-        if (grammars.containsKey(input)) {
-            grammars.put("S", grammars.get("S") + input);
+    /**
+     * add new symbol from input
+     * @param NonTerminal
+     * @param input
+     */
+    public void addSymbol(String NonTerminal, String input) {
+        if (grammars.containsKey(NonTerminal)) {
+            grammars.put(NonTerminal, grammars.get(NonTerminal) + input);
         }
         else {
-            grammars.putIfAbsent("S", input);
+            grammars.putIfAbsent(NonTerminal, input);
         }
+    }
+
+    /**
+     * get the last two symbols of input
+     * @return
+     */
+    public String getDigram() {
+        String previousSymbol = grammars.get("S").substring(grammars.get("S").length()-2);
+        return previousSymbol;
+    }
+
+    public int checkForPattern(String digram) {
+        int count = 0;
+        for (int i = 0; i < grammars.get("S").length()-1; i++) {
+            if (grammars.get("S").substring(i, i + 2).equals(digram)) {
+                count++;
+            }
+        }
+        return count;
+        // check other grammars first
+        // check s
     }
 }

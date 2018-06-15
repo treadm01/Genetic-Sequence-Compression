8f5aa97d16217dea7db028c47c021c4f8d160d35
Toby Readman <treadm01@dcs.bbk.ac.uk>
2018-06-15 13:25:20 +0100

    adding tests for removing bigram

--- Test/NonTerminalTest.java
+++ Test/NonTerminalTest.java
@@ -16,5 +16,32 @@ public class NonTerminalTest {
     }
 
     @Test
+    public void updateBigramsBirgram() {
+        nt.addValues(1);
+        nt.addValues(2);
+        nt.addValues(3);
+        nt.addValues(4);
+        nt.addValues(5);
+        nt.addValues(6);
 
+        nt.getBigramMap();
+    }
+
+    @Test
+    public void removeBigram() {
+        nt.addValues(1);
+        nt.addValues(2);
+        nt.addValues(3);
+        nt.addValues(4);
+        nt.addValues(5);
+        nt.addValues(6);
+        nt.values.remove(3);
+
+        System.out.println(nt.values);
+        System.out.println(nt.bigramMap.size());
+
+        nt.getBigramMap();
+
+        assertEquals(nt.values.size()-1, nt.bigramMap.size());
+    }
 }

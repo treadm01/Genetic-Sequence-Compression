9686ccaec80b83f0889a7c6cac9b1b8f2d130195
Toby Readman <treadm01@dcs.bbk.ac.uk>
2018-07-20 11:38:24 +0100

    trying to add checks for reverse complements

---
+++ Test/ReverseComplementTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReverseComplementTest {
+
+    //a == t and c == g, the swapped values and reverse order
+    // reverse oder first and then switch
+    // so cg = gc convert cg, either either, gc -> swap cg
+    @Test
+    public void simpleReverseComplement() {
+        String input = "acgtcgacgt";
+
+    }
+}
